ec2e1bf14077324f4cce404740ee6f1453c6b4a6
presingigovindraju <141930032+presingigovindraju@users.noreply.github.com>
2023-09-25 16:13:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here
+let regix=/\bthe\b|\ban\b|\ba\b/gi;
 
+console.log(arr.replace(regix,"$"));
+let jan=[];
+let map={};
+for(let i=0;i<sen.length;i++){
+    articellessArray = sen[i].replace(regix," ").trim();
+     jan.push(articellessArray);
+     map[articellessArray] = sen[i];
+}
+
+let ans=[];
+
+for(let i of jan){
+    ans.push(map[i]);
+}
+
+console.log(ans);
